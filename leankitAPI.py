85c3a4b18d0a80625f4c2b32f818aa25e3983cb3
Chad Burkins <chad.burkins@gmail.com>
2017-06-09 07:55:01 -0400

    removed unused code

--- leankitAPI.py
+++ leankitAPI.py
@@ -1,41 +1,20 @@
 #!flask/bin/python
 
+# import libraries from local flask library
 from flask import Flask, jsonify
+# Library for Cross Origin Resource Sharing
+# one-time to load: flask/bin/pip install -U flask-cors
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+# allows CORS for all domains on all routes
 CORS(app)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-def getFishTankTemp (ProbeSerialNum):
-
-    # Output probably looks like this:
-    # b1 01 4b 46 7f ff 0f 10 8d : crc=8d YES
-    # b1 01 4b 46 7f ff 0f 10 8d t=27062
-    # The t= portion is the temp in celcius, in other words 27.062 C
-    # Here's how I original did it:
-    # cat dev-file | grep 't=' | awk -F= '{printf ("(%d/1000*1.8)+32\n", $2)}' | bc -l | awk '{printf ("%.2f\n", $1)}'
-
-    tempF = float(0.0);
-#    ProbeSerialNum='28-00000463f2a4'
-    deviceFile = "/sys/bus/w1/devices/%s/w1_slave" % ProbeSerialNum;
-    with open(deviceFile, 'r') as content_file:
-        for line in content_file:
-            # Grab the line which contains "t="
-            if "t=" in line:
-                # Split the line by '=', get second argument, and convert to integer
-                celcius = int(line.split('=')[1]);
-                # Convert to F
-                tempF = (celcius / 1000.0 * 1.8) + 32
-    return tempF;
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Endpoint for Tank Water Temp
+# Endpoint to list all Leankit cards on our board
 # @app.route is a Python Decorator, modifies the function directly below it
 # Decorators wrap a python function, modifying it's behavior
-
 @app.route('/leankit/list', methods=['GET'])
 def return_fishtank_temp():
     # would like to run this:
@@ -54,17 +33,9 @@ def return_fishtank_temp():
     #del cardsObj[5:]
     return (json.dumps(cardsObj));
 
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-from flask import abort
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+# - - - - - - - - - - - -  Main   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 # make sure we don't run accidentally if we get imported by other python script
 if __name__ == '__main__':
